ba36d80fd191727e44997a2234300a7464354d8d
Cameron <>
2021-04-08 15:32:23 +0000

    swap handle to handle.js and check for top of map

--- index.js
+++ index.js
@@ -9,60 +9,8 @@ const app = express()
 app.use(bodyParser.json())
 
 app.get('/', handle.handleIndex)
-app.post('/start', handleStart)
-app.post('/move', handleMove)
-app.post('/end', handleEnd)
+app.post('/start', handle.handleGameData)
+app.post('/move', handle.handleMove)
+app.post('/end', handle.handleGameData)
 
 app.listen(PORT, () => console.log(`Battlesnake Server listening at http://127.0.0.1:${PORT}`))
-
-function handleStart(request, response) {
-  var gameData = request.body
-
-  console.log('START')
-  response.status(200).send('ok')
-}
-
-function handleMove(request, response) {
-  var gameData = request.body
-
-  var possibleMoves = ['up', 'down', 'left', 'right']
-  var head = gameData.you.head
-  var height = gameData.board.height
-
-  var move = possibleMoves[3]
-    if (moves.canMoveUp(head.y, height)) {
-      move = possibleMoves[0]
-    }
-  //     if (leftIsBlocked) {
-  //       turnRight();
-  //     } else {
-  //       turnLeft();
-  //     }
-  //     while (downIsClear) {
-  //       goDown();
-  //     },
-  //   if (downIsBlocked) {
-  //     if (leftIsBlocked) {
-  //       turnRight();
-  //     } else {
-  //       turnLeft();
-  //     }
-  //     while (upIsClear) {
-  //       goUp();
-  //     }
-  //   }
-  // }]
-  // var move = possibleMoves[Math.floor(Math.random() * possibleMoves.length)]
-
-  console.log('MOVE: ' + move)
-  response.status(200).send({
-    move: move
-  })
-}
-
-function handleEnd(request, response) {
-  var gameData = request.body
-
-  console.log('END')
-  response.status(200).send('ok')
-}
